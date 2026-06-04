40c122162291e078c979f2a356c77d4adf01fbd3
Muhammad Sameer <sameershazad501@gmail.com>
2026-06-04 16:19:24 +0500

    Update versions

--- index.ts
+++ index.ts
@@ -1,11 +1,5 @@
-import 'expo/build/Expo.fx';
 import { registerRootComponent } from 'expo';
-import { activateKeepAwakeAsync } from 'expo-keep-awake';
 
 import App from './App';
-
-if (__DEV__) {
-  activateKeepAwakeAsync();
-}
 
 registerRootComponent(App);
